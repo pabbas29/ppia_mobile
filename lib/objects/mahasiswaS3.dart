b87430261ad05fc5e48f3d94e1179fe193d10a17
Putra <pabbas@gmail.com>
2020-04-12 08:25:08 +0700

    Connect to test API

--- lib/objects/mahasiswaS3.dart
+++ lib/objects/mahasiswaS3.dart
@@ -1,44 +1,31 @@
 class MahasiswaS3 {
-  String _npm;
-  String _namaLengkap;
-  String _namaDepan;
-  String _namaTengah;
-  String _namaBelakang;
-  String _semesterMasuk;
-  String _tahunMasuk;
-  String _matrikulasi;
-  String _latar;
-  String _status;
+  final String id;
+  final String npm;
+  final String namaLengkap;
+  final String namaDepan;
+  final String namaTengah;
+  final String namaBelakang;
+  final String semesterMasuk;
+  final String tahunMasuk;
+  final String matrikulasi;
+  final String latar;
+  final String status;
 
-  MahasiswaS3(this._namaLengkap);
+  MahasiswaS3._({this.id, this.npm, this.namaLengkap, this.namaDepan, this.namaTengah, this.namaBelakang, this.semesterMasuk, this.tahunMasuk, this.matrikulasi, this.latar, this.status});
 
-  getNPM() => this._npm;
-  setNPM(npm) => this._npm = npm;
-
-  getNamaLengkap() => this._namaLengkap;
-
-  getNamaDepan() => this._namaDepan;
-  setNamaDepan(namaDepan) => this._namaDepan = namaDepan;
-
-  getNamaTengah() => this._namaTengah;
-  setNamaTengah(namaTengah) => this._namaTengah = namaTengah;
-
-  getNamaBelakang() => this._namaBelakang;
-  setNamaBelakang(namaBelakang) => this._namaBelakang = namaBelakang;
-
-  getSemesterMasuk() => this._semesterMasuk;
-  setSemesterMasuk(semesterMasuk) => this._semesterMasuk = semesterMasuk;
-
-  getTahunMasuk() => this._tahunMasuk;
-  setTahunMasuk(tahunMasuk) => this._tahunMasuk = tahunMasuk;
-
-  getMatrikulasi() => this._matrikulasi;
-  setMatrikulasi(matrikulasi) => this._matrikulasi = matrikulasi;
-
-  getLatar() => this._latar;
-  setLatar(latar) => this._latar = latar;
-
-  getStatus() => this._status;
-  setStatus(status) => this._status = status;
-
+  factory MahasiswaS3.fromJson(Map<String, dynamic> json) {
+    return new MahasiswaS3._(
+      id: json['ID'],
+      npm: json['NPM'],
+      namaLengkap: json['Nama_Lengkap'],
+      namaDepan: json['Nama_Depan'],
+      namaTengah: json['Nama_Tengah'],
+      namaBelakang: json['Nama_Belakang'],
+      semesterMasuk: json['Semester_Masuk'],
+      tahunMasuk: json['Tahun_Masuk'],
+      matrikulasi: json['Matrikulasi'],
+      latar: json['Latar'],
+      status: json['Status'],
+    );
+  }
 }
--- lib/screens/mahasiswaS3/home.dart
+++ lib/screens/mahasiswaS3/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:ppia_mobile/components/navdrawer.dart';
 import 'package:ppia_mobile/objects/mahasiswaS3.dart';
 
@@ -9,11 +13,33 @@ class ListMahasiswaS3 extends StatefulWidget {
 
 class _ListMahasiswaS3State extends State<ListMahasiswaS3> {
 
-  final List<MahasiswaS3> mahasiswas = [
-    MahasiswaS3('Yulianti'),
-    MahasiswaS3('Musthofa Ridho'),
-    MahasiswaS3('Syarah Susan'),
-  ];
+  List<MahasiswaS3> mahasiswas = List();
+  var isLoading = false;
+
+  void fetchMahasiswaS3() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final response =
+        await http.get('http://aplikasi.fe.ui.ac.id/ppia/api/mahasiswa/S3/read.php?AuthToken=');
+    if (response.statusCode == 200) {
+      mahasiswas = (json.decode(response.body) as List)
+        .map((data) => new MahasiswaS3.fromJson(data))
+        .toList();
+      setState(() {
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMahasiswaS3();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +50,16 @@ class _ListMahasiswaS3State extends State<ListMahasiswaS3> {
         centerTitle: true,
       ),
       drawer: NavDrawer(),
-      body: ListView.builder(
+      body: isLoading ? Center(
+        child: CircularProgressIndicator(),
+        ) 
+        : ListView.builder(
         itemCount: mahasiswas.length,
-        itemBuilder: (context, index) {
+        itemBuilder: (BuildContext context, int index) {
           return Card(
             child: ListTile(
-              title: Text(mahasiswas[index].getNamaLengkap()),
+              title: new Text(mahasiswas[index].namaLengkap),
+              subtitle: new Text(mahasiswas[index].npm),
               contentPadding: null,
               onTap: null, //Tap will direct to detail screen
             )
